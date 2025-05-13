44496033e8c31c3fdffcf7d9e213ae9f099fdd89
Haigan <haiganbr@gmail.com>
2025-05-13 19:36:25 -0300

    Day - layout

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,4 +1,3 @@
-
 export const metadata = {
   title: 'Moteltech',
   description: 'Aplicação geral',
@@ -12,9 +11,15 @@ export default function RootLayout({ children }) {
           rel="stylesheet" 
           href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;500;700&display=swap" 
         />
-        <link 
-      
-        />
+   
+        <script
+          type="module"
+          src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
+        ></script>
+        <script
+          noModule
+          src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
+        ></script>
       </head>
       <body>
         {children}
